a52ef15cc3b8fcadea12e207f14ccde04416e480
Caveman Bian <51192378+bianzheng123@users.noreply.github.com>
2019-09-12 18:13:42 +0800

    update the enter room

---
+++ SUSTechMahjong/Assets/Scripts/NetworkGameUI.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Networking;
+
+public class NetworkGameUI : NetworkBehaviour {
+    private static NetworkGameUI _instance;
+    public static NetworkGameUI Instance
+    {
+        get { return _instance; }
+
+    }//单例
+
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            _instance = this;
+        }
+    }
+
+  
+}
