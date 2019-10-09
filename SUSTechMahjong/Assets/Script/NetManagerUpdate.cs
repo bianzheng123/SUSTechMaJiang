5730846f8e858da0872c201ff50fc6ac26abb013
Caveman Bian <51192378+bianzheng123@users.noreply.github.com>
2019-10-09 15:39:21 +0800

    删除了不必要的代码，并对登陆代码进行了重构

---
+++ SUSTechMahjong/Assets/Script/NetManagerUpdate.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NetManagerUpdate : MonoBehaviour {
+    private void Start()
+    {
+        DontDestroyOnLoad(this);
+
+        if (GameObject.Find("NetManager").gameObject != this.gameObject)
+            Destroy(this.gameObject);
+    }
+
+    void Update () {
+        NetManager.Update();
+        
+
+    }
+}
